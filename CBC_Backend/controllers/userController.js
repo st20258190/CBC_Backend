655573d09ad48b37694fef95b9427f85f903cc86
VenWolf <w.chandarathana99@gmail.com>
2025-07-18 16:19:54 +0530

    Backend Addtionals 😎

--- CBC_Backend/controllers/userController.js
+++ CBC_Backend/controllers/userController.js
@@ -50,6 +50,7 @@ export function loginUser(req, res) {
         res.json({
           token:token,
           message: "login successful",
+          role: user.role
         });
       } else {
         res.status(403).json({
--- CBC_Backend/index.js
+++ CBC_Backend/index.js
@@ -5,32 +5,30 @@ import userRouter from "./routers/userRouter.js";
 import jwt from "jsonwebtoken";
 import productRouter from "./routers/productRouter.js";
 import dotenv from "dotenv";
-dotenv.config()
+import cors from "cors";
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use((req,res,next)=>{
-  const value = req.header("Authorization")
-  if (value!=null) {
-    const token = value.replace("Bearer ","")
-    jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
-      if (decoded==null) {
+app.use(cors());
+app.use((req, res, next) => {
+  const value = req.header("Authorization");
+  if (value != null) {
+    const token = value.replace("Bearer ", "");
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (decoded == null) {
         res.status(403).json({
-          message:"unAuthorized User"
-        })
+          message: "unAuthorized User",
+        });
       } else {
-        
-        req.user=decoded
-        next()
+        req.user = decoded;
+        next();
       }
-
-    })
-  }else{
-    next()
+    });
+  } else {
+    next();
   }
-  
-
-})
+});
 const connectionString = process.env.MONGO_URI;
 mongoose
   .connect(connectionString)
@@ -42,7 +40,7 @@ mongoose
   });
 
 app.use("/api/users", userRouter);
-app.use("/api/products",productRouter)
+app.use("/api/products", productRouter);
 app.listen(4000, () => {
   console.log("Server is Running on port 5000 ");
 });
